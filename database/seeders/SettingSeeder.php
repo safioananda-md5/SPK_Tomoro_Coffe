b2388395e8c7789989617e98f6a551a1cca26274
KHARISMA SAFIO ANANDA <safiopertama@gmail.com>
2026-05-02 12:43:18 +0700

    🛠️🪰Fix Bug: settings seeder

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -13,15 +13,14 @@ class SettingSeeder extends Seeder
      */
     public function run(): void
     {
-        Setting::updateOrCreate(
-            ['id' => 1],
-            [
-                'main_title'   => 'Rekomendasi Terbaik',
-                'main_desc_1'  => 'Eksplorasi rasa kopi...',
-                'main_desc_2'  => 'Dinikmati lebih dari <b>40 juta</b>...',
-                'second_title' => 'Rekomendasi Menu Terbaik',
-                'second_desc'  => 'Pilihan terbaik yang telah teruji...',
-            ]
-        );
+        Setting::updateOrCreate([
+            'id' => 1
+        ], [
+            'main_title' => 'Rekomendasi Terbaik',
+            'main_desc_1' => 'Eksplorasi rasa kopi kelas dunia dengan harga yang bersahabat. Pilih menu favoritmu sekarang dan rasakan standar baru dalam menikmati kopi berkualitas tinggi.',
+            'main_desc_2' => 'Dinikmati lebih dari penikmat Tomoro Coffee di Indonesia sampai detik ini',
+            'second_title' => 'Rekomendasi Menu Terbaik',
+            'second_desc' => 'Pilihan terbaik yang telah teruji. Kami menyeleksi setiap rasa dengan kriteria yang ketat untuk memastikan hanya rasa yang paling dicintai pelanggan yang sampai ke tangan Anda. Dijamin ketagihan!',
+        ]);
     }
 }
